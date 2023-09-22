466e70fd1a7653cafad58a9017fec6aafea02a30
nishly <adhipamungkaswijayadi@gmail.com>
2023-09-22 22:44:28 +0700

    Done Problem 3

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,40 @@
-let num = 3
+let num = 100;
+
+function drawTriangle(num) {
+  let itteration = 1;
+  for (let i = num; i > 0; i--) {
+    let rowString = "";
+
+    for (let j = 0; j < i; j++) {
+      rowString += " ";
+    }
+
+    for (let k = 0; k < itteration; k++) {
+      if (k % 2 === 0) {
+        rowString += "x";
+      } else {
+        rowString += "o";
+      }
+    }
+
+    for (let j = 0; j < i; j++) {
+      rowString += " ";
+    }
+
+    itteration += 2;
+    console.log(rowString);
+  }
+}
+
+console.log(drawTriangle(num));
 
 // Tulis code dibawah sini.
 // dan console log hasil
+//    let rowString = "";
+//    for (let index = 0; index < num * 2; index++) {
+//      if (index - i >= 0 && index + i <= num * 2 - 1) {
+//        rowString += "*";
+//      } else {
+//        rowString += " ";
+//      }
+//    }
