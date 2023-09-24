62b48392560fecf0d913d196e5f6161449870d44
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-09-24 07:59:54 +0700

    Update problem 3

--- problem-3/index.js
+++ problem-3/index.js
@@ -2,3 +2,21 @@ let num = 3
 
 // Tulis code dibawah sini.
 // dan console log hasil
+
+for (let i = 1; i <= num; i++) {
+  let row = '';
+  
+  for (let j = 1; j <= num - i; j++) {
+    row += ' ';
+  }
+
+  for (let k = 1; k <= i * 2 - 1; k++) {
+    if (k % 2 === 1) {
+      row += 'x';
+    } else {
+      row += 'o';
+    }
+  }
+
+  console.log(row);
+}
