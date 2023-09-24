6ff894a9bd8728c1a4a080392daecce6834452fc
AndaraAdisti <142381605+AndaraAdisti@users.noreply.github.com>
2023-09-24 12:41:46 +0700

    problem 8 done
    problem 8

--- problem-8/index.js
+++ problem-8/index.js
@@ -1,5 +1,21 @@
 function cariMedian(arr) {
-    // code disini
+    // Mengurutkan array
+    arr.sort(function(a, b) {
+      return a - b;
+    });
+  
+    // Menghitung panjang array
+    var len = arr.length;
+  
+    // Jika panjang array ganjil, nilai median adalah elemen di tengah
+    if (len % 2 === 1) {
+      return arr[Math.floor(len / 2)];
+    } else {
+      // Jika panjang array genap, nilai median adalah rata-rata dua elemen tengah
+      var mid1 = arr[len / 2 - 1];
+      var mid2 = arr[len / 2];
+      return (mid1 + mid2) / 2;
+    }
   }
   
   // TEST CASES
@@ -8,3 +24,4 @@ function cariMedian(arr) {
   console.log(cariMedian([3, 4, 7, 6, 10])); // 7
   console.log(cariMedian([1, 3, 3])); // 3
   console.log(cariMedian([7, 7, 8, 8])); // 7.5
+  
