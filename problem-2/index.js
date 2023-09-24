6eb6b06605fb1def39f5c478380afe93499124dc
rifqiabdi <rifqiabdillah242@gmail.com>
2023-09-25 03:11:12 +0700

    Mengumpulkan Tugas week 6

--- problem-2/index.js
+++ problem-2/index.js
@@ -1,8 +1,17 @@
 function ladder(word) {
-//your code here
+  //your code here
+  const result = [];
+  for (let a = 0; a < word.length; a++) {
+    const k = [];
+    for (let j = 0; j < word.length - a; j++) {
+      k.push(word[j]);
+    }
+    result.push(k);
+  }
+  return result;
 }
-  
-console.log(ladder('vocasia'));
+
+console.log(ladder("vocasia"));
 //   [
 //     [ 'v', 'o', 'c', 'a', 's', 'i', 'a' ],
 //     [ 'v', 'o', 'c', 'a', 's', 'i' ],
