ca2ae1656a958ef3a11a1d4d452081461e7a04f8
Izzuddin AlFatah <68839136+AlFatah743@users.noreply.github.com>
2023-09-24 19:12:26 +0700

    problem 3 week 6

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,34 @@
-let num = 3
+function generateTriangle(num) {
+  let triangle = '';
+  const maxRowLength = num * 2 - 1;
 
-// Tulis code dibawah sini.
-// dan console log hasil
+  for (let i = 1; i <= num; i++) {
+    // Membuat string kosong untuk menyimpan setiap baris
+    let row = '';
+
+    // Menambahkan spasi sebelum karakter 'x' dan 'o'
+    let spaces = Math.floor((maxRowLength - (i * 2 - 1)) / 2);
+    row += ' '.repeat(spaces);
+
+    // Menambahkan karakter 'x' dan 'o' sesuai dengan pola
+    for (let j = 1; j <= i * 2 - 1; j++) {
+      if (j % 2 === 1) {
+        row += 'x';
+      } else {
+        row += 'o';
+      }
+    }
+
+    // Menambahkan baris ke dalam segitiga
+    triangle += row + '\n';
+  }
+
+  return triangle;
+}
+
+// Contoh penggunaan
+let num1 = 3;
+let num2 = 6;
+
+console.log(generateTriangle(num1));
+console.log(generateTriangle(num2));
