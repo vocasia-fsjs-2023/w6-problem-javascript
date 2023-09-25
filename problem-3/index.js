da35df65bdfe7dd3a30ff20e312e97304d08998e
irhas wira nugraha <107126711+irhaswira11@users.noreply.github.com>
2023-09-25 20:07:24 +0700

    index.js problem 3

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,39 @@
 let num = 3
 
 // Tulis code dibawah sini.
-// dan console log hasil
+for (let i = 0; i < num; i++) {
+  let line = '';
+  
+  for (let j = 0; j < num - i - 1; j++) {
+    line += ' ';
+  }
+  
+  for (let k = 0; k < 2 * i + 1; k++) {
+    if (k % 2 === 0) {
+      line += 'x';
+    } else {
+      line += 'o';
+    }
+  }
+  console.log(line);
+}
+
+console.log("");
+
+let num1 = 6;
+for (let i = 0; i < num1; i++) {
+  let line = '';
+  // Tambahkan spasi sebelum karakter 'x'
+  for (let j = 0; j < num1 - i - 1; j++) {
+    line += ' ';
+  }
+  
+  for (let k = 0; k < 2 * i + 1; k++) {
+    if (k % 2 === 0) {
+      line += 'x';
+    } else {
+      line += 'o';
+    }
+  }
+  console.log(line);
+}
