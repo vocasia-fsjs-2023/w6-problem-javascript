a9a6701a71616b96e068c785f50e6b74d6b3583c
Mikhsn <ihsanpnp56@gmail.com>
2023-10-25 10:32:25 +0700

    done w6 problem

--- problem-4/index.js
+++ problem-4/index.js
@@ -1,17 +1,47 @@
 function changeMe(arr) {
-    // Tulis codemu disini dan console log hasilnya
+  // you can only write your code here!
+  let currentYear = new Date().getFullYear();
+
+  for (let i = 0; i < arr.length; i++) {
+    let firstName = arr[i][0];
+    let lastName = arr[i][1];
+    let gender = arr[i][2];
+    let birthYear = arr[i][3];
+    let age = 'Invalid Birth Year';
+
+    if (birthYear && birthYear <= currentYear) {
+      age = currentYear - birthYear;
+    }
+
+    let personData = {
+      firstName: firstName,
+      lastName: lastName,
+      gender: gender,
+      age: age
+    };
+
+    console.log(`${firstName} ${lastName}:`);
+    console.log(personData);
+  }
+
+  if (arr.length === 0) {
+    console.log('""');
+  }
 }
-  
-  // TEST CASES
-  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]);
   // 1. Christ Evans:
-  // { firstName: 'Christ',
-  //   lastName: 'Evans',
-  //   gender: 'Male',
-  //   age: 38 }
-  // 2. Robert Downey:
-  // { firstName: 'Robert',
-  //   lastName: 'Downey',
-  //   gender: 'Male',
-  //   age: 'Invalid Birth Year' }
-  changeMe([]); // ""
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 38 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
+
+
+  
--- problem-8/index.js
+++ problem-8/index.js
@@ -1,10 +1,18 @@
 function cariMedian(arr) {
-    // code disini
+  // you can only write your code here!
+  let sortedArr = arr.slice();
+  let middleIndex = Math.floor(sortedArr.length / 2);
+
+  if (sortedArr.length % 2 === 0) {
+    return (sortedArr[middleIndex - 1] + sortedArr[middleIndex]) / 2;
+  } else {
+    return sortedArr[middleIndex];
   }
-  
-  // TEST CASES
-  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
-  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
-  console.log(cariMedian([1, 3, 3])); // 3
-  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+}
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
