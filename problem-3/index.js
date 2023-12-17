2af3d0ffd097b841d4d8317004f76831559b53c5
Mohammad Radja Alyfa Amri <116491065+MohammadRadja@users.noreply.github.com>
2023-12-17 20:46:04 +0700

    Problem 3
    Finish Problem 3

--- problem-3/index.js
+++ problem-3/index.js
@@ -2,3 +2,20 @@ let num = 3
 
 // Tulis code dibawah sini.
 // dan console log hasil
+function segitiga(num) {
+    for (let i = 0; i < num; i++) {
+      let baris = '';
+        for (let j = 0; j < num - i - 1; j++){
+            baris += ' ';
+        }
+        for (let k = 0; k < i * 2 + 1; k++){
+            if ( k % 2 === 1){
+                baris += 'x';
+            } else {
+                baris += 'o';
+            }
+        }
+        console.log(baris);
+    }
+}
+segitiga(num);
