d00ac88bd0a324e53c55393414ea279c73a12a0c
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-12-26 18:25:58 +0700

    Update index.js

--- problem-2/index.js
+++ problem-2/index.js
@@ -1,14 +1,17 @@
 function ladder(word) {
-//your code here
+    let result = [];
+
+    for (let i = 0; i < word.length; i++) {
+        let row = [];
+
+        for (let j = 0; j < word.length - i; j++) {
+            row.push(word[j]);
+        }
+
+        result.push(row);
+    }
+
+    return result;
 }
-  
+
 console.log(ladder('vocasia'));
-//   [
-//     [ 'v', 'o', 'c', 'a', 's', 'i', 'a' ],
-//     [ 'v', 'o', 'c', 'a', 's', 'i' ],
-//     [ 'v', 'o', 'c', 'a', 's' ],
-//     [ 'v', 'o', 'c', 'a' ],
-//     [ 'v', 'o', 'c' ],
-//     [ 'v', 'o' ],
-//     [ 'v'],
-//   ]
