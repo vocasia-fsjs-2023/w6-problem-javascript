11b6d3b7db9093cb5d3058d0e1b5636c4fa61a5f
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-12-26 18:26:53 +0700

    Update index.js

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,24 @@
-let num = 3
+function printTriangle(num) {
+    let result = '';
 
-// Tulis code dibawah sini.
-// dan console log hasil
+    for (let i = 1; i <= num; i++) {
+        // Membuat spasi di setiap baris
+        for (let j = 1; j <= num - i; j++) {
+            result += ' ';
+        }
+
+        // Membuat pola 'xox'
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            result += (k % 2 === 1) ? 'x' : 'o';
+        }
+
+        // Pindah ke baris selanjutnya
+        result += '\n';
+    }
+
+    return result;
+}
+
+// Contoh penggunaan
+console.log(printTriangle(3));
+console.log(printTriangle(6));
