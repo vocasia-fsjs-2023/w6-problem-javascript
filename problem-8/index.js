b5a6c477b491935c624e0c135882c6b80a403c0e
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-12-26 18:44:26 +0700

    Update index.js

--- problem-8/index.js
+++ problem-8/index.js
@@ -1,10 +1,23 @@
 function cariMedian(arr) {
-    // code disini
-  }
-  
-  // TEST CASES
-  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
-  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
-  console.log(cariMedian([1, 3, 3])); // 3
-  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+    // Mengurutkan array
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+
+    var tengah = Math.floor(arr.length / 2);
+
+    // Jika jumlah elemen ganjil
+    if (arr.length % 2 !== 0) {
+        return arr[tengah];
+    } else {
+        // Jika jumlah elemen genap, hitung rata-rata dua elemen tengah
+        return (arr[tengah - 1] + arr[tengah]) / 2;
+    }
+}
+
+// Test cases
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
